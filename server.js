5d63a8c51e4f1aa6bb72d6b366c7a649e054aed7
liujiani505 <64924326+liujiani505@users.noreply.github.com>
2021-09-18 21:54:00 -0400

    finished index route

--- server.js
+++ server.js
@@ -2,10 +2,21 @@ const express = require("express");
 const { ppid } = require("process");
 const app = express();
 const port = 3000;
+const budgets = require("./models/budget")
+
+///////////////////////////
+// Middleware
+///////////////////////////
+app.use(express.static("public"))
+
+
+///////////////////////////
+// Routes
+///////////////////////////
 
 // INDEX ROUTE
 app.get("/budgets", (req, res)=>{
-
+    res.render("index.ejs", {allBudgets: budgets})
 })
 
 // NEW ROUTE
@@ -15,12 +26,14 @@ app.get("/budgets/new", (req, res)=>{
 
 // CREATE ROUTE
 app.post("/budgets", (req, res)=>{
-    
+
 })
 
 // SHOW ROUTE
 app.get("/budgets/:index", (req, res)=>{
 
 })
+
+
 
 app.listen(port);
